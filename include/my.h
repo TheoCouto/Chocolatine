3c7c9b4c5c3dcaee6701b121cbd5a92d39490c4e
Theo Couto Christofidis <theo.couto-christofidis@epitech.eu>
2024-04-05 18:31:41 +0200

    feat: recover all files

---
+++ include/my.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Intro to Devops: Chocolatine
+** File description:
+** header file
+*/
+
+#ifndef MY_H_
+    #define MY_H_
+    #include <stdlib.h>
+
+int error_handling(int argc, char **argv);
+
+#endif /* MY_H_ */
